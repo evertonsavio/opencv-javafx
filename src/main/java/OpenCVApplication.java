54d300dc9040f9ff6b4a1820f25c2bbf59104aab
evertonsavio <everluca@outlook.com>
2022-06-18 14:19:45 -0300

    :sparkles: opencv working

---
+++ src/main/java/OpenCVApplication.java
@@ -0,0 +1,44 @@
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
+
+public class OpenCVApplication
+{
+    public static void main(String[] args)
+    {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        Mat loadedImage = loadImage("./public/jd.png");
+        MatOfRect facesDetected = new MatOfRect();
+
+        CascadeClassifier cascadeClassifier = new CascadeClassifier();
+        int minFaceSize = Math.round(loadedImage.rows() * 0.1f);
+        cascadeClassifier.load("./src/main/resources/haarcascades/haarcascade_frontalface_alt.xml");
+        cascadeClassifier.detectMultiScale(loadedImage,
+                facesDetected,
+                1.1,
+                3,
+                Objdetect.CASCADE_SCALE_IMAGE,
+                new Size(minFaceSize, minFaceSize),
+                new Size()
+        );
+
+        Rect[] facesArray = facesDetected.toArray();
+        for(Rect face : facesArray) {
+            Imgproc.rectangle(loadedImage, face.tl(), face.br(), new Scalar(0, 0, 255), 3);
+        }
+        saveImage(loadedImage, "./public/jds.png");
+    }
+
+    public static Mat loadImage(String imagePath) {
+        Imgcodecs imageCodecs = new Imgcodecs();
+        return imageCodecs.imread(imagePath);
+    }
+
+    public static void saveImage(Mat imageMatrix, String targetPath) {
+        Imgcodecs imgcodecs = new Imgcodecs();
+        imgcodecs.imwrite(targetPath, imageMatrix);
+    }
+}
